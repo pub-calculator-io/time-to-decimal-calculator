8094b9775a646965ea45e2317b2574487f15861f
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:14:23 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_time_to_decimal_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Time to Decimal Calculator by Calculator.iO";
 
 function display_ci_time_to_decimal_calculator(){
